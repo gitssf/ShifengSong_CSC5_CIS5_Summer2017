0d951b227f52e1e0a3fed6c7aa2936e532b189b2
gitssf <ssong9@student.rccd.edu>
2017-07-21 16:40:45 -0700

    6

--- Project_1/Blackjack/nbproject/Makefile-impl.mk
+++ Project_1/Blackjack/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Project_1
+PROJECTNAME=Blackjack
 
 # Active Configuration
 DEFAULTCONF=Debug
--- Project_1/Blackjack/nbproject/Makefile-variables.mk
+++ Project_1/Blackjack/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
-CND_ARTIFACT_NAME_Debug=project_1
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/project_1
+CND_ARTIFACT_NAME_Debug=blackjack
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/blackjack
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
-CND_PACKAGE_NAME_Debug=project1.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/project1.tar
+CND_PACKAGE_NAME_Debug=blackjack.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/blackjack.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
-CND_ARTIFACT_NAME_Release=project_1
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/project_1
+CND_ARTIFACT_NAME_Release=blackjack
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/blackjack
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
-CND_PACKAGE_NAME_Release=project1.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/project1.tar
+CND_PACKAGE_NAME_Release=blackjack.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/blackjack.tar
 #
 # include compiler specific variables
 #
